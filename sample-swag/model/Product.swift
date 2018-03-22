963ba5722e2e712390ebb70c9f200765d144eef2
Pooya <pooya@MacBook-Air.local>
2018-03-22 17:30:59 +0430

    Prepare data model.

--- sample-swag/model/Product.swift
+++ sample-swag/model/Product.swift
@@ -1,5 +1,5 @@
 //
-//  Category.swift
+//  Product.swift
 //  sample-swag
 //
 //  Created by Pooya on 1/2/1397 AP.
@@ -7,15 +7,15 @@
 //
 
 import Foundation
-struct Category{
-
+struct Product {
     private(set) public var title: String
+    private(set) public var price: String
     private(set) public var imageName: String
     
-    init(title: String, imageName: String) {
-        self.title = title
+    init(title:String, price: String, imageName: String) {
         self.imageName = imageName
+        self.price = price
+        self.title = title
     }
     
 }
-
--- sample-swag/services/DataService.swift
+++ sample-swag/services/DataService.swift
@@ -19,9 +19,52 @@ class DataService{
         Category(title: "Digital", imageName: "digital.png")
     ]
     
+    private let hats = [
+        Product(title: "Hat 01", price: "5$", imageName: "hat01.png"),
+        Product(title: "Hat 02", price: "6$", imageName: "hat02.png"),
+        Product(title: "Hat 03", price: "8$", imageName: "hat03.png"),
+        Product(title: "Hat 04", price: "12$", imageName: "hat04.png")
+    ]
+    
+    private let shirts = [
+        Product(title: "Shirt 01", price: "53$", imageName: "shirt01.png"),
+        Product(title: "Shirt 02", price: "62$", imageName: "shirt02.png"),
+        Product(title: "Shirt 03", price: "81$", imageName: "shirt03.png"),
+        Product(title: "Shirt 04", price: "42$", imageName: "shirt04.png"),
+        Product(title: "Shirt 05", price: "34$", imageName: "shirt05.png")
+    ]
+    
+    
+    private let hoodies = [
+        Product(title: "Hoodie 01", price: "53$", imageName: "hoodie01.png"),
+        Product(title: "Hoodie 02", price: "62$", imageName: "hoodie02.png"),
+        Product(title: "Hoodie 03", price: "81$", imageName: "hoodie03.png"),
+        Product(title: "Hoodie 04", price: "42$", imageName: "hoodie04.png")
+    ]
+    
+    private let digitalGoods = [Product]()
+    
     func getCategories() -> [Category]{
         return categories
     }
+    
+    
+    func getProducts(forCategoryName category:String) -> [Product] {
+        switch category {
+        case "Hoodies":
+            return self.hoodies
+        case "Hats":
+            return self.hats
+        case "Shirts":
+            return self.shirts
+        default:
+            return self.shirts
+        }
+    }
+    
+    
+    
+    
     
     
 
