ec707bf717b1503ef939a16cbdbc85d6ab038e44
Pooya <pooya@MacBook-Air.local>
2018-03-22 16:35:38 +0430

    Product cell is designed.

---
+++ sample-swag/view/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  sample-swag
+//
+//  Created by Pooya on 1/2/1397 AP.
+//  Copyright © 1397 Binogoo. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var productTitle: UILabel!
+    
+    
+}
