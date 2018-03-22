e45e7be52b2f2f5901e0c317ab0c9eb84bf0f86c
Pooya <pooya@MacBook-Air.local>
2018-03-22 14:22:40 +0430

    Table view cell is added

---
+++ sample-swag/view/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  sample-swag
+//
+//  Created by Pooya on 1/2/1397 AP.
+//  Copyright © 1397 Binogoo. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
